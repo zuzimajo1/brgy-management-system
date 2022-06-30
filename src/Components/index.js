2b6b8755bd0719783aa96112ba8286ef646d4ee4
Zuzim <ajozuzim>
2022-06-30 14:45:43 +0800

    logo

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,3 +1,4 @@
 export { default as HeaderContainer } from "./HeaderContainer";
 export { default as NavbarContainer } from "./NavbarContainer";
 export { default as ColorScheme } from "./ColorScheme";
+export { default as Logo } from "./Logo";
