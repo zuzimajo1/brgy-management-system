04191fd496dd38a8626ea77791d832e0aab3351f
Zuzim <ajozuzim>
2022-07-04 23:02:47 +0800

    updating default functions

--- src/Components/index.js
+++ src/Components/index.js
@@ -2,3 +2,6 @@ export { default as HeaderContainer } from "./HeaderContainer";
 export { default as NavbarContainer } from "./NavbarContainer";
 export { default as ColorScheme } from "./ColorScheme";
 export { default as Logo } from "./Logo";
+export { default as WebCamera } from "./WebCamera";
+export { default as RegisterForm } from "./RegisterForm";
+export { default as FaceRecognitionWebCam } from "./FaceRecognitionWebCam";
