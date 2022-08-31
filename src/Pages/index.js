f26913414d14315cdaee186c89e5fba085a5d961
Zuzim <ajozuzim>
2022-08-31 21:21:28 +0800

    add report in indexjs

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -3,3 +3,4 @@ export { default as Home } from "./Home";
 export { default as Documents } from "./Documents";
 export { default as Auth } from "./Auth";
 export { default as Masterlist } from "./Masterlist";
+export { default as Report } from "./Report";
